de0d428c1c6d0e0ce732b4cd3c16fcbaa0e3e68d
izebit <izebit@gmail.com>
2017-04-11 00:56:43 +0300

    excluded spring mongo autoconfiguration

--- src/main/java/ru/izebit/config/BaseConfiguration.java
+++ src/main/java/ru/izebit/config/BaseConfiguration.java
@@ -1,9 +1,11 @@
 package ru.izebit.config;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {MongoAutoConfiguration.class, MongoDataAutoConfiguration.class})
 @ComponentScan(basePackages = "ru.izebit")
 public class BaseConfiguration {
 
